efd7746287759b2faa14c06759cd0285802f7472
jonas5 <jo@live.no>
2025-08-25 07:10:27 +0100

    .

---
+++ build.cmd
@@ -0,0 +1,3 @@
+REM Build the project using MSBuild
+"C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin\MSBuild.exe" dxwrapper.sln /m /p:Configuration=Release /p:Platform=Win32
+
